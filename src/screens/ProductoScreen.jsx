fb5864062e8e1e2f6fba720e34d1ed62d2d1e936
Aaron-Rojas <201002327+Aaron-Rojas@users.noreply.github.com>
2026-05-27 00:26:05 -0500

    Se implemento el FlastList, para ello se tuvo que: - Agregar más productos dentro del json - En el componenten, agrupar las Card, para que se renderizen dentro del FlatList

--- src/screens/ProductoScreen.jsx
+++ src/screens/ProductoScreen.jsx
@@ -1,10 +1,9 @@
-import { SafeAreaView, ScrollView, StyleSheet, ActivityIndicator} from 'react-native';
+import { SafeAreaView, View, StyleSheet, ActivityIndicator, Text, FlatList} from 'react-native';
 import React, { useState } from 'react';
 
 import ProductTitle from '../components/products/ProductTitle';
 import ProductSearch from '../components/forms/ProductSearch';
 import CardProduct from '../components/products/CardProduct';
-import NavBar from '../components/ui/NavBar';
 
 //Importamos nuestro hooks a usar
 import { useProductos } from '../hooks/useProductos';
@@ -15,36 +14,33 @@ export default function ProductoScreen({ navigation }) {
   //Extramoes lo que usará el hook
   const { productos, cargando, error } = useProductos();
 
+  //Agrupamos el título y buscador para que el FlatList los ponga arriba
+  const renderHeader = () => (
+    <View>
+      <ProductTitle />
+      <ProductSearch busqueda={busqueda} setBusqueda={setBusqueda} />
+      {cargando && <ActivityIndicator size="large" color="#4A1C20" style={{marginTop: 20}} />}
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
         
-        <ProductTitle />
-        
-        <ProductSearch 
-          busqueda={busqueda} 
-          setBusqueda={setBusqueda} 
-        />
-
-        {/* Pantalla de Carga  */}
-        {cargando && <ActivityIndicator size="large" color="#4A1C20" />}
-        
-        {/* Si en caso, falla */}
-        {error && <Text style={styles.errorText}>{error}</Text>}
-
-        {!cargando && !error && productos.map((item) => (
-              <CardProduct 
-                key={item.id}
-                titulo={item.nombre} 
-                precio={item.precio} 
-                stock={item.stock} 
-                imagenUrl={item.imagenUrl} 
-              />
-            ))}
-        
-
-      </ScrollView>
-
+        <FlatList
+        data={!cargando && !error ? productos: []} 
+        keyExtractor={
+          (item) => item.id.toString()}
+          ListHeaderComponent={renderHeader}
+          contentContainerStyle={styles.scrollContainer}
+          renderItem={({item}) => 
+          <CardProduct
+            titulo={item.nombre} 
+            precio={item.precio} 
+            stock={item.stock} 
+            imagenUrl={item.imagenUrl}
+          /> 
+        }/>
     </SafeAreaView>
   );
 }
